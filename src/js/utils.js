90ec6d0a5e9d89e8f41908b38e1fdf0cd086922f
dankorotenko <danilkorotenko2@gmail.com>
2022-12-23 00:05:21 +0100

    markdown layout + image plugin

---
+++ src/js/utils.js
@@ -0,0 +1,13 @@
+export function slugify(text) {
+  return text
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+}
+export function formatDate(date) {
+  return new Date(date).toLocaleDateString();
+}
